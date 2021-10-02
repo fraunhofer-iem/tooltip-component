817bac6bc30e0dd56da137725fbe75efcc2f3b5e
Felix <felix2510@gmx.net>
2021-10-02 14:55:36 +0200

    Updated exported members

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import { tippyfy } from './components/TooltipComponent';
-export type { TippyControl } from './components/TooltipComponent';
+export type { TooltipControl } from './components/TooltipComponent';
 export default tippyfy;
